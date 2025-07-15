6b09fe111c8133b19223f1d0e069ba21cf968069
Gopichand Mogaparthi <gopichandmogaparthi@Mac.attlocal.net>
2025-07-14 22:32:48 -0500

    🚀 Fix anchor IDs & smooth scroll

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -18,10 +18,10 @@ const Navbar = () => {
 
   const navLinks = [
     { name: "Home", href: "/" },
-    { name: "About", href: "/about" },
-    { name: "Skills", href: "/skills" },
-    { name: "Projects", href: "/projects" },
-    { name: "Contact", href: "/contact" },
+    { name: "About", href: "#about" },
+    { name: "Skills", href: "#skills" },
+    { name: "Projects", href: "#projects" },
+    { name: "Contact", href: "#contact" },
   ];
 
   return (
